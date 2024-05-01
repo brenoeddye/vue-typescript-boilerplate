d2d99645ea0f448bbf222706a4cdb4f62f763f78
Breno Pereira <brenopereira856@gmail.com>
2024-05-01 18:09:05 -0300

    feat: add base components and remove default vue components

---
+++ src/components/base/Button.vue
@@ -0,0 +1,70 @@
+<script lang="ts">
+export default defineComponent({
+    props: {
+        type: String,
+        href: String,
+        icon: String,
+        blank: Boolean,
+    }
+})
+</script>
+
+<template>
+    <router-link 
+        v-if="href"
+        :class="'btn ' + type" 
+        :to="href" 
+        :target="blank ? '_blank' : ''">
+        <baseIcon 
+            v-if="icon"
+            class="icon"
+            :icon="icon"
+            width="24" height="24"/>
+
+        <slot />
+    </router-link>
+
+    <button v-else :class="'btn ' + type">
+        <baseIcon 
+            v-if="icon"
+            class="icon"
+            :icon="icon"
+            width="24" height="24"/>
+        <slot />
+    </button>
+</template>
+
+<style lang="scss" scoped>
+.btn {
+    background-color: transparent;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 0;
+    color: var(--font-color);
+    border-radius: 8px;
+    padding: .7em 1em;
+    transition: all .25s ease-in-out;
+    cursor: pointer;
+
+    .icon {
+        margin-right: 8px;
+    }
+
+    &.primary {
+        background-color: var(--primary);
+
+        &:hover {
+            background-color: var(--primary-hover);
+        }
+    }
+
+    &.secondary {
+        background-color: var(--primary-bg-hover);
+
+        &:hover {
+            background-color: var(--primary-bg-active);
+        }
+    }
+}
+</style>
---
+++ src/components/base/Icon.vue
@@ -0,0 +1,37 @@
+<script lang="ts">
+import icons from '@/assets/icons/icons.json';
+
+export default defineComponent({
+    props: {
+        icon:       String,
+        width:      String,
+        height:     String,
+        colors:     String,
+        clickable:  Boolean
+    },
+    computed: {
+        iconData() {
+            return icons.find((item) => item.icon === this.icon) || null;
+        },
+        iconSource() {
+            return this.iconData ? this.iconData.data : null;
+        },
+    }
+})
+</script>
+
+<template>
+    <i class="app-icon" v-if="iconSource" :style="{ backgroundImage: `url(${iconSource})`, width: `${width}px`, height: `${height}px` }" :class="colors"></i>
+</template>
+
+<style lang="scss" scoped>
+.app-icon {
+    background-size: 100%;
+    display: flex;
+    filter: invert(100%) sepia(0%) saturate(7478%) hue-rotate(40deg) brightness(105%) contrast(105%);
+
+    &.primary {
+        filter: invert(32%) sepia(96%) saturate(1618%) hue-rotate(207deg) brightness(78%) contrast(93%);
+    }
+}
+</style>
